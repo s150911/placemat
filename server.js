58b212d1cb69c9ef6bcd95b176679a57be863dc8
stbaeumer <baeumer@posteo.de>
2018-12-22 12:50:51 +0100

    make it better

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
+import express, { static } from 'express';
+import { urlencoded } from 'body-parser';
+import { createConnection } from 'mysql';
 
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(static('public'))
+app.use(urlencoded({extended: true}))
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-var con = mysql.createConnection({
+var con = createConnection({
     host: "130.255.124.99",    
     user: "placematman",
     password: "BKB123456!",
