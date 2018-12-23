14afc93f8b40f31519562ac0833d8fa55644abd4
student999 <student999@berufskolleg-borken.de>
2018-12-23 12:48:41 +0100

    make it better

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
+const con = mysql.createConnection({
+    host: "130.255.124.99",    
+    user: "placematman",
+    password: "BKB123456!",
+    database: "dbPlacemat"
+})
+
+con.connect(function(err){
+
+    if (err) throw err
+
+    console.log("Erfolgreich mit der Datenbank verbunden.")
+
+    con.query("SELECT * from placemat ORDER BY nummer DESC LIMIT 1;", function(err, result1) {
+        
+        if (err) throw err;
+        
+        // Falls kein Fehler auftritt, wird der Erfolg geloggt.
+        
+        console.log("Tabelle 'taetigkeit' erfolgreich angelegt, bzw. schon vorhanden.");
+    })
+})
+
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
