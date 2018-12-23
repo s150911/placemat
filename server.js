d477b73f3d123040aacaabf0d518b782bc1cae1a
student999 <student999@berufskolleg-borken.de>
2018-12-23 11:16:02 +0100

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -10,13 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
-})
-
-var con = mysql.createConnection({
-    host: "130.255.124.99",    
-    user: "placematman",
-    password: "BKB123456!",
-    database: "dbPlacemat"
 })
 
 app.get('/',function(req,res){    
