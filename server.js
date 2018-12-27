662adac616c1dbaee5f14968f506bca0f3695c7d
student999 <student999@berufskolleg-borken.de>
2018-12-27 09:32:25 +0100

    ...

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
@@ -10,12 +11,15 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const mysql = require('mysql')
 const dbVerbindung = mysql.createConnection({
-    host: "130.255.124.99", user: "placematman", password: "BKB123456!", database: "dbPlacemat"
+    host: "130.255.124.99", 
+    user: "placematman", 
+    password: "BKB123456!", 
+    database: "dbPlacemat"
 })
 dbVerbindung.connect()
 
 app.get('/',(req,res) => {    
-    dbVerbindung.query("SELECT thema from placemat;", (err, rows, fields) => {        
+    dbVerbindung.query("SELECT thema from placemat;", (err, rows) => {        
         res.render('index.ejs', {        
             anzeigen: [rows[0].thema, "Jetzt beitreten!"]
         })
@@ -23,14 +27,14 @@ app.get('/',(req,res) => {
 })
 
 app.post('/', (req,res) => {    
-    dbVerbindung.query("SELECT * from placemat;", (err, rows, fields) => {
+    dbVerbindung.query("SELECT * from placemat;", (err, rows) => {
         let placematUsers = []   
         let thema = rows[0].thema        
         let anzahlGruppen = rows[0].anzahlGruppen
         let endeUhrzeitThink = rows[0].endeUhrzeitThink
         let endeUhrzeitPair = rows[0].endeUhrzeitPair
                
-        dbVerbindung.query("SELECT * from placematuser;", (err, rows, fileds) => {         
+        dbVerbindung.query("SELECT * from placematuser;", (err, rows) => {         
             if (err) throw err
             for (let row of rows) {                
                 let placematUser = new PlacematUser()
@@ -66,7 +70,11 @@ app.post('/', (req,res) => {
             }
 
             dbVerbindung.query("INSERT INTO placematuser(gruppe, name, thema, treffpunkt) VALUES ('" + placematUser.gruppe + "','" + placematUser.name + "','" + placematUser.thema + "','" + placematUser.treffpunkt + "');", (err, result) => {
-                let anzeigen = ["Hallo " + placematUser.name + "!","Du bist in der " + placematUser.gruppe + ". Gruppe ", "THINK hat bereits begonnen.", "PAIR ab " + ("0" + endeUhrzeitThink.getHours()).slice(-2) +":" + ("0" + endeUhrzeitThink.getMinutes()).slice(-2) + " Uhr bei " + placematUser.treffpunkt + ".", "PAIR endet um " + ("0" + endeUhrzeitPair.getHours()).slice(-2) +":" + ("0" + endeUhrzeitPair.getMinutes()).slice(-2) + " Uhr."]
+                let anzeigen = ["Hallo " + placematUser.name + "!"]
+                anzeigen.push("Du bist in der " + placematUser.gruppe + ". Gruppe ") 
+                anzeigen.push("THINK hat bereits begonnen.") 
+                anzeigen.push("PAIR ab " + ("0" + endeUhrzeitThink.getHours()).slice(-2) +":" + ("0" + endeUhrzeitThink.getMinutes()).slice(-2) + " Uhr bei " + placematUser.treffpunkt + ".") 
+                anzeigen.push("PAIR endet um " + ("0" + endeUhrzeitPair.getHours()).slice(-2) +":" + ("0" + endeUhrzeitPair.getMinutes()).slice(-2) + " Uhr.") 
                 anzeigen.push("Viel Spaß :-)")                
                 res.render('index.ejs', {        
                     anzeigen: anzeigen                        
@@ -132,14 +140,7 @@ app.post('/admin', (req,res) => {
         if (err) throw err        
     })
 
-    dbVerbindung.query("INSERT INTO placemat(thema, zeitstempel, anzahlGruppen, dauerThink, endeUhrzeitThink, dauerPair, endeUhrzeitPair) VALUES ('" + req.body.tbxThema + "', now(), '" + req.body.tbxAnzahlGruppen + "','" + req.body.tbxThink + "', ADDTIME(now(), '0:" + req.body.tbxThink + ":0'),'" + req.body.tbxPair + "', ADDTIME(now(), '0:" + (parseInt(req.body.tbxPair) + parseInt(req.body.tbxThink)) + ":0'));", (err, result) => {
-    
-        let footnote = "Placemat erfolgreich angelegt: Thema: " + req.body.tbxThema + " AnzahlGruppen:" + req.body.tbxAnzahlGruppen + " Think:" + req.body.tbxThink + " Pair:" + req.body.tbxPair
-
-        if (err) {
-            footnote = err.message
-        }                
-                     
+    dbVerbindung.query("INSERT INTO placemat(thema, zeitstempel, anzahlGruppen, dauerThink, endeUhrzeitThink, dauerPair, endeUhrzeitPair) VALUES ('" + req.body.tbxThema + "', now(), '" + req.body.tbxAnzahlGruppen + "','" + req.body.tbxThink + "', ADDTIME(now(), '0:" + req.body.tbxThink + ":0'),'" + req.body.tbxPair + "', ADDTIME(now(), '0:" + (parseInt(req.body.tbxPair) + parseInt(req.body.tbxThink)) + ":0'));", (err, result) => {                     
         res.render('admin.ejs', {
             thema: req.body.tbxThema,
             anzahlGruppen: req.body.tbxAnzahlGruppen,
