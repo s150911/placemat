0a53bda3dc0fd7b65ada9b75ff310f0e2ef7d043
student999 <student999@berufskolleg-borken.de>
2018-12-27 07:56:06 +0100

    ...

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
